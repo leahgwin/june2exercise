424f3751121df28fe2b5e8afd15503854349f067
leahgwin <leahgwin@gmail.com>
2018-06-04 09:09:25 -0500

    completed exercise

---
+++ main.js
@@ -0,0 +1,8 @@
+// Exercise: Using Javascript, create a variable to hold your last name. Then, if your last name has 7 or more letters, console log "I can't pronounce this last name". If your last name has less than 7 characters, console log your last name.
+// Challenge: Create a new repository on Github and push the code your wrote up to Github.
+
+var lastName = "Gwin";
+
+if (lastName.length < 7) {
+    console.log(lastName);
+}
